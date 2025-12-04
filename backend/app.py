d24d589f3809168218fec5aefb4b851351b41e56
Dawood Sarfraz <dawoodsarfraz.cs@gmail.com>
2025-12-04 16:47:31 +0500

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI # type: ignore
+from fastapi.responses import JSONResponse, FileResponse # type: ignore
+from schema.user_input import UserInput
+from schema.prediction_response import PredictionResponse
+from model.predict import predict_output, model, MODEL_VERSION
+
+app = FastAPI()
+
+# human readable       
+@app.get('/')
+def home():
+    return {'message':'Insurance Premium Prediction API'}
+
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse("favicon.ico")  # put a favicon.ico in the same folder
+
+# machine readable
+@app.get('/health')
+def health_check():
+    return {
+        'status': 'OK',
+        'version': MODEL_VERSION,
+        'model_loaded': model is not None
+    }
+
+@app.post('/predict', response_model=PredictionResponse)
+def predict_premium(data: UserInput):
+
+    user_input = {
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }
+
+    try:
+
+        prediction = predict_output(user_input)
+
+        return JSONResponse(status_code=200, content={'response': prediction})
+    
+    except Exception as e:
+
+        return JSONResponse(status_code=500, content=str(e))
+
+
+
+
+
